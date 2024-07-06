a6dfd92ef28452c566550a287a3a1a80aee14ca5
om <ashuomg7352@gmail.com>
2024-07-06 13:27:53 +0530

    Quiz

---
+++ quiz/src/main/java/com/quiz/quiz/controller/QuizController.java
@@ -0,0 +1,37 @@
+package com.quiz.quiz.controller;
+
+import com.quiz.quiz.model.Question;
+import com.quiz.quiz.model.QuestionWrapper;
+import com.quiz.quiz.model.Response;
+import com.quiz.quiz.service.QuizService;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+// controller for quiz
+@RestController
+@RequestMapping("quiz")
+public class QuizController {
+
+    @Autowired
+    QuizService quizService;
+
+    @PostMapping("create")
+    public ResponseEntity<String> createQuiz(@RequestParam String category, @RequestParam int numQ, @RequestParam String title){
+        return quizService.createQuiz(category, numQ, title);
+    }
+    @GetMapping("get/{id}")
+    public ResponseEntity<List<QuestionWrapper>> getQuizQuestions(@PathVariable Integer id){
+        return quizService.getQuizQuestions(id);
+    }
+
+    @PostMapping("submit/{id}")
+    public ResponseEntity<Integer> submitQuiz(@PathVariable Integer id, @RequestBody List<Response> responses){
+        return quizService.calculateResult(id, responses);
+    }
+
+
+}
---
+++ quiz/src/main/java/com/quiz/quiz/model/Quiz.java
@@ -0,0 +1,47 @@
+package com.quiz.quiz.model;
+
+import jakarta.persistence.*;
+import lombok.Data;
+
+import java.util.List;
+
+@Entity
+@Data
+public class Quiz {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Integer id;
+    private String title;
+
+    @ManyToMany
+    private List<Question> questions;
+
+    //getters & setters
+	public Integer getId() {
+		return id;
+	}
+
+	public void setId(Integer id) {
+		this.id = id;
+	}
+
+	public String getTitle() {
+		return title;
+	}
+
+	public void setTitle(String title) {
+		this.title = title;
+	}
+
+	public List<Question> getQuestions() {
+		return questions;
+	}
+
+	public void setQuestions(List<Question> questions) {
+		this.questions = questions;
+	}
+    
+    
+
+}
